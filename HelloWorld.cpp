300ae65bae0619742d7636e1821297052c8c46a1
Mehul_Manani <mehul.gecr@gmail.com>
2024-02-09 11:09:27 +0500

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+// A program to display Hello World.
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    std::cout<<"Hello World!";
+    std::cout<<"\nEnter the value of a: \n";
+    std::cin>>a;
+    std::cout<<"The value of a is : "<<a;
+    std::cout<<"\nEnd of Program";
+    return 0;
+}
+
+
